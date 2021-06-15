6b09f66e37212f6d83c86509a7240b505e48c622
luke33559767 <3359767@gmail.com>
2021-06-15 10:38:46 -0400

    0.0.1 update

--- nhentaiHelper/popup.js
+++ nhentaiHelper/popup.js
@@ -1,4 +1,30 @@
-let words = document.getElementById(`detact_text`);
-let regrex = /\d\d\d\d\d\d/gi;
-let numbers = words.values.match(regrex);
-console.log(numbers);
+document.addEventListener("DOMContentLoaded", documentEvents, false);
+let sixDigit;
+
+function myAction(input) {
+  sixDigit=input.value.match(/\d{6}/);
+  if(!sixDigit){return}
+  sixDigitLink=`https://nhentai.net/g/${sixDigit}`;
+//   alert(sixDigitLink);
+   $.ajax({
+     crossOrigin:true,
+     url: sixDigitLink,
+     type: "GET",
+     success: function (data) {
+        alert('0990')
+       $("#content").html($(data).find("#content").html());
+     },
+   });
+  
+  // do processing with data
+  // you need to right click the extension icon and choose "inspect popup"
+  // to view the messages appearing on the console.
+}
+
+function documentEvents() {
+  document.getElementById("detact").addEventListener("click", function () {
+    myAction(document.getElementById("detact_text"));
+  });
+
+  // you can add listeners for other objects ( like other buttons ) here
+}
